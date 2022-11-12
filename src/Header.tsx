4ae1424f1499e651b6022464c9aaf6bc1d3dc202
mayakovsky <mayakovsky03@gmail.com>
2022-11-12 15:42:58 +0200

    add Header and Technologies practice components

---
+++ src/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Header = () => {
+    return (
+        <>
+            <a href="#s">Home</a>
+            <a href="#s">News</a>
+            <a href="#s">Feed</a>
+            <a href="#s">Messages</a>
+        </>
+    )
+}
+
