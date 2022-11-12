b4be16047be18931a09bd9015151e9c854fab2e6
mayakovsky <mayakovsky03@gmail.com>
2022-11-12 18:45:36 +0200

    add new components Navbar, Profile and Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,16 @@
 import React from 'react';
 import './App.css';
 import {Header} from "./components/Header";
+import {Navbar} from "./components/Navbar";
+import {Profile} from "./components/Profile";
 
 
 function App() {
     return (
         <div className='app-wrapper'>
             <Header/>
-            <nav className='nav'>
-                <a href={''}>Profile</a>
-                <a href={''}>Messages</a>
-                <a href={''}>News</a>
-                <a href={''}>Music</a>
-                <a href={''}>Settings</a>
-            </nav>
-            <div className='content'>
-                <div>
-                    <img src="https://wallpaperaccess.com/full/11861.jpg" alt="Picture"/>
-                </div>
-                <div>
-                    avatar + description
-                </div>
-                <div>
-                    my post
-                    <div>
-                        new post
-                    </div>
-                </div>
-                <div>post 1</div>
-                <div>post 2</div>
-            </div>
+            <Navbar/>
+            <Profile/>
         </div>
     );
 }
---
+++ src/components/Profile.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src="https://i.pinimg.com/564x/10/3e/39/103e39b7d430b671138040029cf366af.jpg" alt="Picture"/>
+            </div>
+            <div>
+                avatar + description
+            </div>
+            <div>
+                my post
+                <div>
+                    new post
+                </div>
+            </div>
+            <div>post 1</div>
+            <div>post 2</div>
+        </div>
+    )
+}
