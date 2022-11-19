9090f8d38734541b340cdab73afce276e66dbce2
mayakovsky <mayakovsky03@gmail.com>
2022-11-19 23:35:20 +0300

    19. add routing components Music, News, Settings

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import {Navbar} from "./components/Navbar/Navbar";
 import {Profile} from "./components/Profile/Profile";
 import {Dialogs} from "./components/Dialogs/Dialogs";
 import {BrowserRouter, Route} from "react-router-dom";
+import {News} from "./components/News/News";
+import {Music} from "./components/Music/Music";
+import {Settings} from "./components/Settings/Settings";
 
 
 function App() {
@@ -16,6 +19,9 @@ function App() {
                 <div className='app-wrapper-content'>
                     <Route path={'/messages'} component={Dialogs}/>
                     <Route path={'/profile'} component={Profile}/>
+                    <Route path={'/news'} component={News}/>
+                    <Route path={'/music'} component={Music}/>
+                    <Route path={'/settings'} component={Settings}/>
                 </div>
             </div>
         </BrowserRouter>
