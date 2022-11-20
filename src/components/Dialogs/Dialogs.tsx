d6ba46bf06419024325e7070a8e4af1a5b51b0f9
mayakovsky <mayakovsky03@gmail.com>
2022-11-20 15:06:56 +0300

    19. add new component Dialogs and new class active

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,8 +3,18 @@ import s from './Dialogs.module.css';
 
 export const Dialogs = () => {
     return (
-        <div className={s.div}>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>dimych</div>
+                <div className={s.dialog + ' ' + s.active}>andry</div>
+                <div className={s.dialog + ' ' + s.active}>vovich</div>
+                <div className={s.dialog + ' ' + s.active}>sema</div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>yo</div>
+                <div className={s.message}>hey</div>
+                <div className={s.message}>hei hei</div>
+            </div>
         </div>
     )
 };
